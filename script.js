f275eb9ba61f7a712e6e00daba0c949cd1eaade7
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-10 20:39:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,12 +3,14 @@ let text = document.getElementById("text");
 let delay = document.getElementById("delay");
 
 let output = document.getElementById("output");
+let output = document.getElementById("output");
+
 
 function getData(dataId){
 	return new Promise((resolve,reject)=>{
 		setTimeOut(()=> {
 			output.innerText = text;
-		}, 3000);	
+		}, delay);	
 	});
 }
 
