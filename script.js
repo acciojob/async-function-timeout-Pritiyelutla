47c7a32ffb40b66c8e10eaad5d2ba08b077906be
Pritiyelutla <143998125+Pritiyelutla@users.noreply.github.com>
2024-06-10 20:31:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let text = document.getElementById("text");
+let delay = document.getElementById("delay");
+
+let output = document.getElementById("output");
+
+function getData(dataId){
+	return new Promise((resolve,reject)=>{
+		setTimeOut(()=> {
+			output.innerText = text;
+		}, 3000);	
+	});
+}
+
+async callGetdata(){
+	await getData();
+}
